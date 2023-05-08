526857e02a682b4e76812f673a7f705b929e1aa5
Jeremy Childers <30885417+jlchilders11@users.noreply.github.com>
2023-05-08 17:00:47 -0400

    Fixes a bug present on windows, which does not support the delete key… (#1)
    * Switches a mistaken usage of the internal Django implementation of NamedTemporaryFile to the python implementation of NamedTemporaryFile

--- wagtail_wordpress_import/block_builder_defaults.py
+++ wagtail_wordpress_import/block_builder_defaults.py
@@ -1,8 +1,8 @@
 import requests
+from tempfile import NamedTemporaryFile
 from bs4 import BeautifulSoup
 from django.conf import settings
 from django.core.files import File
-from django.core.files.temp import NamedTemporaryFile
 from django.utils.module_loading import import_string
 from wagtail.documents import get_document_model
 from wagtail.images import get_image_model
